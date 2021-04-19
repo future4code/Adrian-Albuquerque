946f777f7177cf4be31bdf3c6564a9280b68d1c5
Adrian Américo <1fnadrian1@gmail.com>
2021-04-19 15:36:24 -0300

    Atividade 10 de Javascript

---
+++ semana-3/variaveis/index.js
@@ -0,0 +1,39 @@
+// const nome = "Adrian";
+// const sobreNome = "Américo";
+// let idade = 19;
+// console.log(`meu nome é ${nome} ${sobreNome}, e tenho ${idade} anos !`)
+// // alert("test")
+// let teste = [1, 3, 6];
+// console.log(teste[0])
+
+// let desafio = [1, 2, 3, 4, 5, 6];
+// console.log(desafio.length)
+// console.log (desafio[0], desafio[2], desafio[desafio.length - 1])
+
+
+
+// a) Declare uma variável para armazenar um nome, sem atribuir um valor.
+
+// b) Declare uma variável para armazenar uma idade, sem atribuir um valor.
+
+// c) Imprima no console o tipo dessas variáveis que acabou de criar, usando o comando `typeof`.
+
+// d) Reflita: por que esse tipo foi impresso? Escreva a resposta em um comentário de código.
+
+// d) Pergunte ao usuário seu nome e sua idade, atribuindo esses dois valores às variáveis que acabou de criar.
+
+// Dica: se você receber um erro de código nessa etapa, reflita sobre o tipo de declaração que utilizou.
+
+// let nome;
+// let idade;
+// console.log(typeof(nome)) //undefined
+// //Não foi declrado um valor, pro isso indefinido !
+// nome = prompt("Digite seu nome")
+// console.log(nome)
+// idade = prompt("Digite sua idade")
+// console.log(idade)
+// alert('Bem vindo(a) ' + nome + ' Você tem  ' + idade + ' anos !')
+//imprime no console nome e idade do usuario
+
+// Faça um programa que faça 5 perguntas para o usuário (pode ser criativo aqui). 
+// Imprima-as com as respostas no console da seguinte forma:
