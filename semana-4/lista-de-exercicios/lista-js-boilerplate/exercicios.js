c31f307dc1cecb4906784c1a7bfe724cdd96ff48
Adrian Américo <1fnadrian1@gmail.com>
2021-04-30 17:57:46 -0300

    atividade 2

--- semana-4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
+++ semana-4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
@@ -1,21 +1,32 @@
 //Exercício 1
 
 function inverteArray(array) {
-  // implemente sua lógica aqui
-  let arrayInvertido = array.splice(0).reverse()
-  return arrayInvertido
+   const arrayInvertido = []
+   for (let i = array.length - 1; i >= 0; i--) {
+      arrayInvertido.push(array[i])
+   }
+   return arrayInvertido
 }
 
 //Exercício 2
 
-function retornaNumerosParesElevadosADois (array) {
+function retornaNumerosParesElevadosADois(array) {
    // implemente sua lógica aqui
-   
+   let arrayPar = [];
+   for (let i = 0; i <= array.length; i++) {
+      if (array[i] % 2 === 0) {
+         arrayPar.push(array[i])
+      }
+   }
+   for(let i = 0;i < arrayPar.length; i++) {
+      arrayPar[i] = arrayPar[i] ** 2
+   }
+   return arrayPar
 }
 
 //Exercício 3
 
-function retornaNumerosPares (array) {
+function retornaNumerosPares(array) {
    // implemente sua lógica aqui
 }
 
@@ -27,7 +38,7 @@ function retornaMaiorNumero(array) {
 
 //Exercício 5
 
-function retornaQuantidadeElementos (array) {
+function retornaQuantidadeElementos(array) {
    // implemente sua lógica aqui
 }
 
@@ -46,7 +57,7 @@ function retornaNNumerosPares(n) {
 // Exercício 8
 
 function checaTriangulo(a, b, c) {
-  // implemente sua lógica aqui
+   // implemente sua lógica aqui
 }
 
 // Exercício 9
@@ -93,11 +104,22 @@ function anonimizaPessoa(pessoa) {
 
 // Exercício 16
 
-const arrayDePessoas = [
-  { nome: "Pedro", idade: 20 },
-  { nome: "João", idade: 10 },
-  { nome: "Paula", idade: 12 },
-  { nome: "Artur", idade: 89 } 
+const arrayDePessoas = [{
+      nome: "Pedro",
+      idade: 20
+   },
+   {
+      nome: "João",
+      idade: 10
+   },
+   {
+      nome: "Paula",
+      idade: 12
+   },
+   {
+      nome: "Artur",
+      idade: 89
+   }
 ]
 
 // Exercício 16, letra A
@@ -121,7 +143,7 @@ function multiplicaArrayPor2(array) {
 // Exercício 17, letra B
 
 function multiplicaArrayPor2S(array) {
-  // implemente sua lógica aqui
+   // implemente sua lógica aqui
 }
 
 // Exercício 17, letra C
@@ -132,13 +154,36 @@ function verificaParidade(array) {
 
 // Exercício 18
 
-const pessoas = [
-  { nome: "Paula", idade: 12, altura: 1.8},
-  { nome: "João", idade: 20, altura: 1.3},
-  { nome: "Pedro", idade: 15, altura: 1.9},
-  { nome: "Luciano", idade: 22, altura: 1.8},
-  { nome: "Artur", idade: 10, altura: 1.2},
-  { nome: "Soter", idade: 70, altura: 1.9}
+const pessoas = [{
+      nome: "Paula",
+      idade: 12,
+      altura: 1.8
+   },
+   {
+      nome: "João",
+      idade: 20,
+      altura: 1.3
+   },
+   {
+      nome: "Pedro",
+      idade: 15,
+      altura: 1.9
+   },
+   {
+      nome: "Luciano",
+      idade: 22,
+      altura: 1.8
+   },
+   {
+      nome: "Artur",
+      idade: 10,
+      altura: 1.2
+   },
+   {
+      nome: "Soter",
+      idade: 70,
+      altura: 1.9
+   }
 ]
 
 //Exercício 18, letra A
@@ -156,43 +201,88 @@ function retornaPessoasNaoAutorizadas() {
 
 //Exercício 19
 
-const consultasNome = [
-   { nome: "João", dataDaConsulta: "01/10/2021" },
-   { nome: "Pedro", dataDaConsulta: "02/07/2021" },
-   { nome: "Paula", dataDaConsulta: "03/11/2021" },
-   { nome: "Márcia",  dataDaConsulta: "04/05/2021" }
- ]
- 
- //Exercício 19, letra A
- 
- function ordenaPorNome() {
-  
- }
- 
- // Exercício 19, letra B
- 
- const consultasData = [
-   { nome: "João", dataDaConsulta: "01/10/2021" },
-   { nome: "Pedro", dataDaConsulta: "02/07/2021" },
-   { nome: "Paula", dataDaConsulta: "03/11/2021" },
-   { nome: "Márcia",  dataDaConsulta: "04/05/2021" }
- ]
- 
- function ordenaPorData() {
- 
- }
+const consultasNome = [{
+      nome: "João",
+      dataDaConsulta: "01/10/2021"
+   },
+   {
+      nome: "Pedro",
+      dataDaConsulta: "02/07/2021"
+   },
+   {
+      nome: "Paula",
+      dataDaConsulta: "03/11/2021"
+   },
+   {
+      nome: "Márcia",
+      dataDaConsulta: "04/05/2021"
+   }
+]
+
+//Exercício 19, letra A
+
+function ordenaPorNome() {
+
+}
+
+// Exercício 19, letra B
+
+const consultasData = [{
+      nome: "João",
+      dataDaConsulta: "01/10/2021"
+   },
+   {
+      nome: "Pedro",
+      dataDaConsulta: "02/07/2021"
+   },
+   {
+      nome: "Paula",
+      dataDaConsulta: "03/11/2021"
+   },
+   {
+      nome: "Márcia",
+      dataDaConsulta: "04/05/2021"
+   }
+]
+
+function ordenaPorData() {
+
+}
 
 //Exercício 20
 
-const contas = [
-  { cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
-  { cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
-  { cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
-  { cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
-  { cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
-  { cliente: "Soter", saldoTotal: 1200, compras: [] }
+const contas = [{
+      cliente: "João",
+      saldoTotal: 1000,
+      compras: [100, 200, 300]
+   },
+   {
+      cliente: "Paula",
+      saldoTotal: 7500,
+      compras: [200, 1040]
+   },
+   {
+      cliente: "Pedro",
+      saldoTotal: 10000,
+      compras: [5140, 6100, 100, 2000]
+   },
+   {
+      cliente: "Luciano",
+      saldoTotal: 100,
+      compras: [100, 200, 1700]
+   },
+   {
+      cliente: "Artur",
+      saldoTotal: 1800,
+      compras: [200, 300]
+   },
+   {
+      cliente: "Soter",
+      saldoTotal: 1200,
+      compras: []
+   }
 ]
 
 function atualizaSaldo() {
-  // implemente sua lógica aqui
+   // implemente sua lógica aqui
 }
