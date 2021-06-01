3522bf355c06f1a56591d6111a9d5999f6592128
Adrian Américo <1fnadrian1@gmail.com>
2021-06-01 16:36:46 -0300

    PokeApiHooks

---
+++ semana-9/useEffect/pokeapi/src/Components/PokeCard/PokeCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PokeCard(props) {
+    return (
+        <div>
+            <select>
+
+            </select>
+        </div>
+    )
+}
+export default PokeCard;
