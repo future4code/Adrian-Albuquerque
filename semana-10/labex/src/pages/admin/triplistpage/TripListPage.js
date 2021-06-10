31a1ce747d6d638ae748a539f363c0c9495a4819
Adrian Américo <1fnadrian1@gmail.com>
2021-06-10 12:42:11 -0300

    debug na requisicao de exibicao da lista de candidatos

--- semana-10/labex/src/pages/admin/triplistpage/TripListPage.js
+++ semana-10/labex/src/pages/admin/triplistpage/TripListPage.js
@@ -1,25 +1,22 @@
-import React, { useState, useEffect } from 'react';
 import { goToLastPage } from '../../coordinator';
 import { useHistory, useParams } from 'react-router-dom';
 import { useListTripsDetails } from '../../../requests/Request';
+import useProtectedPage from '../../../hooks/useProtectedPage';
 
 function TripListPage() {
+    useProtectedPage()
     const params = useParams();
     const history = useHistory();
     const tripList = useListTripsDetails([], params.id);
-    const [travelCandidates, setTravelCandidates] = useState([])
 
     const tripMap = [tripList]
     console.log(tripList.candidates)
-    
 
-    useEffect(() => {
-        console.log(travelCandidates)
-    }, [])
     return (
         <div>
             {
-                tripMap ? tripMap.map(res => {
+                tripMap && tripMap.map(res => {
+
                     return <div>
                         <h2>Viagem para {res.planet}</h2>
                         <p>Nome: {res.name}</p>
@@ -32,10 +29,20 @@ function TripListPage() {
                         <h2>CANDIDATOS APROVADOS</h2>
                         <p>{res.approved}</p>
                     </div>
-
-                }) : <p>Carregando</p>
+                })
             }
-    
+
+            {tripList && tripList.candidates && tripList.candidates.map(resul => {
+                console.log(resul)
+                return (<div key={resul.id}>
+                    <p>{resul.name}</p>
+                    <p>{resul.profession}</p>
+                    <p>{resul.age}</p>
+                    <p>{resul.country}</p>
+                    <p>{resul.applicationText}</p>
+                </div>)
+            })}
+
             <button onClick={() => goToLastPage(history)}>Voltar</button>
         </div>
     )
