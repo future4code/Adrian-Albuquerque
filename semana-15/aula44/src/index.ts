b067eab40e04101c069f5e546ea1cfc605c8d7c3
Adrian Américo <1fnadrian1@gmail.com>
2021-07-15 20:54:37 -0300

    finalizado tasks

--- semana-15/aula44/src/index.ts
+++ semana-15/aula44/src/index.ts
@@ -1,4 +1,12 @@
 import { app } from "./app";
+import { getTypeUsers } from "./endpoint/getTypeUsers";
 import { getUsers } from "./endpoint/getUsers";
+import { getUserByName } from "./endpoint/getUserByName";
+import { postNewUser } from "./endpoint/postNewUser";
+import { putEditUsers } from "./endpoint/putEditUsers";
 
 app.get("/users", getUsers);
+app.get("/users/type", getTypeUsers);
+app.get("/users/search/:name", getUserByName);
+app.post("/users", postNewUser);
+app.put("/users/edit/:id", putEditUsers);
