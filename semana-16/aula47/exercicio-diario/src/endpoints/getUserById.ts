e56b3ac86ebc72c16069f3b52f99c271239b51fb
Adrian Américo <1fnadrian1@gmail.com>
2021-07-21 17:56:50 -0300

    corrigido erro do getUserById que retornava mesmo nao havendo usuario no banco de dados

--- semana-16/aula47/exercicio-diario/src/endpoints/getUserById.ts
+++ semana-16/aula47/exercicio-diario/src/endpoints/getUserById.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import connection from "../connection";
+import validateUserId from "../functions/validadeUserId";
 
 const getUserById = async (req: Request, res: Response) => {
   let errorCode = 400;
@@ -10,6 +11,13 @@ const getUserById = async (req: Request, res: Response) => {
       errorCode = 422;
       throw new Error("ID not a number");
     }
+
+    const userValidate = validateUserId(id);
+    if (!userValidate) {
+      errorCode = 404;
+      throw new Error("User not found");
+    }
+
     const result = await connection.raw(`SELECT * FROM Actor WHERE ${id} = id`);
     res.status(200).send(result[0]);
   } catch (error) {
