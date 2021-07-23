c4a287a94dbbd82bbadf11d86c14c9c71824af6a
Adrian Américo <1fnadrian1@gmail.com>
2021-07-23 10:22:34 -0300

    retornar todos os usuarios funcional

--- semana-16/projeto/src/index.ts
+++ semana-16/projeto/src/index.ts
@@ -1,4 +1,6 @@
 import app from "./app";
+import getAllUsers from "./endpoints/getAllUsers";
 import postCreateuser from "./endpoints/postCreateUser";
 
+app.get("/", getAllUsers)
 app.post("/create", postCreateuser);
