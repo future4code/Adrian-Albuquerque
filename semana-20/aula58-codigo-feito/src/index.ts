ad45547270e0c9d3bdefd1580768e433258ee1ff
Adrian Américo <1fnadrian1@gmail.com>
2021-08-16 22:34:54 -0300

    atualizado index

--- semana-20/aula58-codigo-feito/src/index.ts
+++ semana-20/aula58-codigo-feito/src/index.ts
@@ -3,10 +3,11 @@ import { getTaskById } from "./endpoints/getTaskById";
 import { login } from "./endpoints/login";
 import { signupController } from "./controller/users/signupController";
 import { createTaskController } from "./controller/tasks/createTaskController";
+import { allUsersController } from "./controller/users/allUsers.controller";
 
 app.post("/users/signup", signupController);
 app.post("/users/login", login);
-app.get("/all");
+app.get("/all", allUsersController);
 
 app.post("/tasks", createTaskController);
 app.get("/tasks/:id", getTaskById);
