1e15a828739da62e0b1b3ee5116eacc481cb7cd7
Adrian Américo <1fnadrian1@gmail.com>
2021-08-16 22:33:59 -0300

    pegar todos os usuarios

--- semana-20/aula58-codigo-feito/src/index.ts
+++ semana-20/aula58-codigo-feito/src/index.ts
@@ -1,11 +1,12 @@
-import { app } from "./app"
-import { getTaskById } from './endpoints/getTaskById'
-import { login } from './endpoints/login'
-import { signupController } from "./controller/users/signupController"
-import { createTaskController } from "./controller/tasks/createTaskController"
+import { app } from "./app";
+import { getTaskById } from "./endpoints/getTaskById";
+import { login } from "./endpoints/login";
+import { signupController } from "./controller/users/signupController";
+import { createTaskController } from "./controller/tasks/createTaskController";
 
-app.post('/users/signup', signupController)
-app.post('/users/login', login)
+app.post("/users/signup", signupController);
+app.post("/users/login", login);
+app.get("/all");
 
-app.post('/tasks', createTaskController)
-app.get('/tasks/:id', getTaskById)
+app.post("/tasks", createTaskController);
+app.get("/tasks/:id", getTaskById);
